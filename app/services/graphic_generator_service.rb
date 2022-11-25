5cdaa154fcc3d4895ff5bb907fa13a4ad7334754
Rodrigo Toledo <rodrigo@Rodrigos-MacBook-Pro.local>
2022-11-24 21:11:53 -0300

    feature/graphics - generator of graphics using highchart export

--- app/services/graphic_generator_service.rb
+++ app/services/graphic_generator_service.rb
@@ -58,26 +58,35 @@ class GraphicGeneratorService
   end
 
   def self.sample
-    require 'json'
     require 'uri'
     require 'net/http'
-    url = URI('http://export.highcharts.com/')
-    http = Net::HTTP.new(url.host, url.port)
-    request = Net::HTTP::Post.new(url)
-    request['Content-Type'] = 'application/json'
-    request.body = '{"infile":{"title": {"text": "Steep Chart"}, "xAxis": {"categories": ["Jan", "Feb", "Mar"]}, "series": [{"data": [29.9, 71.5, 106.4]}]}}'
-    response = http.request(request)
+    # url = URI('http://export.highcharts.com/')
+    # http = Net::HTTP.new(url.host, url.port)
+    # request = Net::HTTP::Post.new(url)
+    # request['Content-Type'] = 'application/json'
+    # request.body = '{"infile":{"title": {"text": "Steep Chart"}, "xAxis": {"categories": ["Jan", "Feb", "Mar"]}, "series": [{"data": [29.9, 71.5, 106.4]}]}}'
+    # response = http.request(request)
+    # puts response.read_body
     # puts "response #{res.body}"
 
-    # res = Net::HTTP.post_form(uri,
-    #                           'type' => 'png',
-    #                           'width' => 600,
-    #                           # 'options' => options,
-    #                           'globaloptions' => data)
+    global_options = { infile: { title: { text: 'Steep Chart' }, xAxis: { categories: %w[Jan Feb Mar] },
+                                 series: [{ data: [29.9, 71.5, 106.4] }] } }
+    # data = { type: 'png', width: 600, globalOptions: global_options }
+    # response = Net::HTTP.post_form(url, global_options)
+
+    uri = URI.parse('http://export.highcharts.com/')
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    data = { globalOptions: global_options }
+    request = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
+    request.body = { infile: { title: { text: 'Steep Chart' }, xAxis: { categories: %w[Jan Feb Mar] },
+                               series: [{ data: [29.9, 71.5, 106.4] }] } }.to_json
+
+    response = http.request(request)
 
     file = Rails.root.join('tmp', File.basename("#{SecureRandom.urlsafe_base64}.png"))
-    # puts res.read_body
-    File.write(file.to_s, response.body)
+    puts response.body
+    # File.write(file.to_s, response.body)
 
     # file_pdf = Rails.root.join('tmp', File.basename("#{SecureRandom.urlsafe_base64}grafico_glicemia.pdf"))
     # Prawn::Document.generate(file_pdf.to_s, page_layout: :landscape) do
@@ -91,5 +100,30 @@ class GraphicGeneratorService
     # GraphicPrawnMailer.send_file(file_pdf.to_s).deliver_now
     # File.delete(file.to_s)
     # File.delete(file_pdf.to_s)
+  end
+
+  def self.generator
+    require 'net/http'
+    require 'uri'
+
+    uri = URI.parse('http://export.highcharts.com')
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = 'application/json'
+
+    chart_options = { infile: { title: { text: 'Roda essa treta' }, xAxis: { categories: %w[Jan Feb Mar] },
+                                series: [{ data: [29.9, 71.5, 106.4] }] } }.to_json
+
+    request.body = chart_options
+
+    req_options = {
+      use_ssl: uri.scheme == 'https'
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    file = Rails.root.join('tmp', File.basename("#{SecureRandom.urlsafe_base64}.png"))
+    File.write(file.to_s, response.body.force_encoding('UTF-8'))
   end
 end
